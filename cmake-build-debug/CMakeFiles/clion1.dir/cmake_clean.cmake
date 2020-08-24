2332fed994f26f22e742aec79518882ce3aef8cd
paulo.correa <paulo.correa@indt.org.br>
2020-08-24 09:32:04 -0400

    initial comminting to pcorrea_base start

---
+++ cmake-build-debug/CMakeFiles/clion1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion1.dir/main.cpp.o"
+  "clion1"
+  "clion1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/clion1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
